57549a74705489a02fbba363178c5dbbf91dfe86
Guillaume Viot <guillaume.gvt@gmail.com>
2013-09-02 02:09:12 +0200

    Added a test for circular buffers's transition when reaching end of allocated zone

--- makefile
+++ makefile
@@ -3,6 +3,6 @@ all:
 	@echo 'This makefile is only used with the "test" argument to unit test C code'
 
 test:
-	gcc -o tests/buffer.out tests/buffer.c src/circular_buffer.c -lcheck -lpython
+	gcc -o tests/buffer.out tests/buffer.c src/circular_buffer.c -lcheck -lpython -std=c99
 	./tests/buffer.out
 
--- tests/buffer.c
+++ tests/buffer.c
@@ -10,8 +10,36 @@ START_TEST (test_write_grows_size)
 	circular_buffer b;
 	buffer_init(&b);
 	ck_assert(b.read_cursor.length == 0);
+	size_t initial_available_size = b.write_cursor.length;
 	buffer_printf(&b, "Let's grow some %s !", "bytes");
+	size_t new_available_size = b.write_cursor.length;
 	ck_assert(b.read_cursor.length > 0);
+	ck_assert(initial_available_size > new_available_size);
+}
+END_TEST
+
+START_TEST (test_reach_end_of_buffer)
+{
+	circular_buffer b;
+	const char *buf;
+	int len, written, total_written;
+
+	buffer_init(&b);
+	const char * const dummy_text = "DUMMY_TEXT...";
+	mark_point();
+	while (total_written < (CIRCULAR_BUFFER_SIZE * 3 + 100)) {
+		written = buffer_printf(&b, "%s%s", dummy_text, dummy_text);
+		total_written += written;
+		buffer_readable_bytes(&b, &buf, &len);
+		buffer_consume_bytes(&b, len);
+		ck_assert_int_eq(len, (strlen(dummy_text) * 2));
+		ck_assert_int_eq(written, (strlen(dummy_text) * 2));
+		ck_assert_int_gt(b.write_cursor.length, -1);
+		ck_assert_int_eq(b.data + CIRCULAR_BUFFER_SIZE, (b.write_cursor.position + b.write_cursor.length));
+		ck_assert_int_gt(CIRCULAR_BUFFER_SIZE, (b.write_cursor.position-b.data));
+		mark_point();
+	}
+	mark_point();
 }
 END_TEST
 
@@ -21,6 +49,7 @@ buffer_suite (void)
 	Suite *s = suite_create("Circular Buffer");
 	TCase *tc_write = tcase_create("write");
 	tcase_add_test(tc_write, test_write_grows_size);
+	tcase_add_test(tc_write, test_reach_end_of_buffer);
 	suite_add_tcase(s, tc_write);
 	return s;
 }
